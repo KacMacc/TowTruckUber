e8b0c2a70604259c08bcb0c09515e355dc2e5573
Krystian97 <krystianmateja@yahoo.pl>
2021-05-18 20:11:14 +0200

    Payment IDs send to backend

--- TowTruckUberAPI/Models/Dtos/PaymentDto.cs
+++ TowTruckUberAPI/Models/Dtos/PaymentDto.cs
@@ -7,9 +7,12 @@ namespace TowTruckUberAPI.Models.Dtos
 {
     public class PaymentDto
     {
+        public int Id { get; init; }
         public string State { get; init; }
         public string Name { get; init; }
-        public string Description { get; set; }
-        public UserDto Payer { get; set; }
+        public string StartDate { get; init; }
+        public string Description { get; init; }
+        public String PayerId { get; init; }
+        public Object Plan { get; init; }
     }
 }
